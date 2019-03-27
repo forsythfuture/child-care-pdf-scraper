136b92b65ae854baa33e809ae726f350ca55d44c
shanejorr <shane.j.orr@gmail.com>
2019-03-27 14:54:18 +0000

    complete coordinates

--- scrape_pdf.R
+++ scrape_pdf.R
@@ -3,11 +3,7 @@ library(tidyverse)
 
 url <- 'https://ncchildcare.ncdhhs.gov/Portals/0/documents/pdf/S/statistical_detail_report_january_2019.pdf'
 
-all_names <- list(c(111.5828, 328.8344, 139.8439, 587.0828))
-
-extract_tables(url, pages = 2, area = all_names, guess = F)
-
-locate_areas(url, pages = 1)
+locate_areas(url, pages = 3)
 
 # coordinates to extract --------------------
 
@@ -25,8 +21,6 @@ lr_children <- c(310.31, 588.05)
 lr_operation <- c(592, 677.7134)
 lr_site <- c(663.5962, 773.5751)
 lr_scc <- c(771.2675, 794.6561)
-
-cols <- list(c(7.24, 71.56, 261.59, 310.31, 592, 663.59, 771.26))
 
 # create a single lsit of all left right coordinates to iterate through
 lr_coords <- list(lr_id, lr_name, lr_month, lr_children, lr_operation, lr_site, lr_scc)
@@ -47,6 +41,8 @@ extract_block <- function(lr, top_row) {
   # height of each row
   row_height <- 28
   
+  # initialize list to store coordinates
+  coords <- list()
   
   # iterate through rows, extracting coordinates of each row
   for (i in seq_len(num_rows)) {
@@ -71,6 +67,22 @@ coord_blocks <- map(lr_coords, extract_block, top_row = top_row) %>%
   # flatten list so each element is a different block
   flatten()
 
-table <- extract_tables(url, pages = rep(1,length(coord_blocks)), 
-                        area = coord_blocks, guess = F,
+# pull isolated blocks: county, date, type (center / home)
+county <- c(43.32598,  71.90792,  57.32867, 283.65267)
+date <- c(30.97066, 686.54443,  46.62073, 728.56381)
+type <- c(44.97335, 347.91761,  58.97604, 473.97577)
+
+# the reports contain county summary pages after each county's listing of individual homes
+# create an extract that extracts a portion of the summary page, to check and see if the 
+# page is a summary page; if it is, we will skip pulling from the page
+# this extract will contain the word 'ALL' if the page is a summary page
+summary_page <- c(581.194078,   4.347334, 595.196770,  28.240711)
+
+# append isolated blocks to the end of the coordinates list
+coord_blocks <- list.append(coord_blocks, county, date, type, summary_page)
+
+c()
+
+table <- extract_tables(url, pages = rep(seq(1, 900), each = length(coord_blocks)), 
+                        area = rep(coord_blocks, 900), guess = F,
                         method = 'lattice')
