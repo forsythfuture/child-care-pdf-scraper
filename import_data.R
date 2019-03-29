8e6146e77c7501178bb55d0379d40cb439ddc6f0
shanejorr <shane.j.orr@gmail.com>
2019-03-29 18:37:20 +0000

    update section for skipping pages

--- import_data.R
+++ import_data.R
@@ -7,15 +7,21 @@
 library(tidyverse)
 library(tabulizer)
 
+#url <- "https://ncchildcare.ncdhhs.gov/Portals/0/documents/pdf/S/statistical_detail_report_january_2019.pdf"
+
+#locate_areas(url, 1)
+
 source('scrapper_functions.R')
 
 # enter year that we want to extract data for
-year <- 2018
+year <- 2019
 
 # each PDF file is a different month,
 # so we will iterate through months, extracting files
 # month names must be lower case, because they are lower case in file names
-months <- str_to_lower(month.name)[9:12]
+#months <- str_to_lower(month.name)[9:12]
+
+months <- 'january'
 
 # each monthly file is at a different url, with a consistent style
 # the string below represents the base URL, with month and year added at the end
@@ -25,7 +31,11 @@ url_base <- "https://ncchildcare.ncdhhs.gov/Portals/0/documents/pdf/S/statistica
 coords <- list(c(106.92, 5.18, 546.07, 797.49), # table with data
                c(42.69, 71.78, 57.87, 303.17), # county
                c(45.02, 355.76, 59.04, 477.30), # type
-               c(579.94, 1.66, 597.46, 40.22)) # test for summary
+               # test for whether it is a detail page we want to extract
+               # this block will equal 'Child Care Analysis Detail' if it is
+               c(31.67, 290.82, 46.28, 532.50)) 
+               #c(579.94, 1.66, 597.46, 40.22), # test for summary 1
+               #c(564.73, 2.36, 580.32,34.52)) # test for summary 2
 
 # iterate through each month, which is a different PDF file
 for (month in months) {
@@ -55,8 +65,8 @@ for (month in months) {
                                  area = coords, guess = F,
                                  method = 'lattice')
     
-    # skip county summary pages, which will have 'ALL' under 4
-    if (table_text[[4]][,1] == "ALL") next
+    # skip pages that don't have the details per facility
+    if (table_text[[4]][,1] != "Child Care Analysis Detail") next
     
     single_month <- ff_create_df(table_text) %>%
       bind_rows(single_month, .)
