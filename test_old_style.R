16972c6a6f060ce40502fdc912e16c5deecde06a
shanejorr <shane.j.orr@gmail.com>
2019-04-12 16:14:43 +0000

    update

--- test_old_style.R
+++ test_old_style.R
@@ -10,13 +10,14 @@ download.file(file_path, temp)
 
 locate_areas(file_path, pages = 1)
 
-coords <- list(c(125.63363, 24.31169, 678.16460, 70.55844), # stars
-               c(125.63363, 67.55844, 688.67788, 493.01299), # middle: name to category
-               c(125.6336, 493.0130, 680.5009, 584.1818)) # operation site and scc
+coords <- list(c(95, 10, 700, 88), # stars
+               c(95, 88, 700, 887), # middle: name to category
+               c(95, 887, 700, 1095)) # operation site and scc
 
-full <- extract_tables(file_path, pages = rep(1, length(coords)), 
+full <- extract_tables(temp, pages = rep(14, length(coords)), 
                        area = coords, guess = F, method = 'lattice')
+
+full
 
 pdf_text('test_page.pdf')
 
-pdf_subset(temp, pages = 1, output = 'test_page.pdf')
